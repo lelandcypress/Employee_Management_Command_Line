5c515b59f9b162307c83a729ceb520b6d5351f85
Michael Souter <souterm1@gmail.com>
2021-07-26 22:46:18 -0400

    Main Menu built, three queries plugged in and functioning, need to get return to main menu function working to make development easier.

--- index.js
+++ index.js
@@ -1,7 +1,65 @@
+const { listenerCount } = require("events");
+const { prompt } = require("inquirer");
 const inquirer = require("inquirer");
 
-const { selectAllEmployees } = require("./query");
+const {
+  selectAllEmployees,
+  selectAllDepartments,
+  selectAllRoles,
+} = require("./query");
 
-selectAllEmployees();
+const initInquirer = () =>
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "navMenu",
+        message: "Welcome! Please Select Option",
+        choices: [
+          "View all Employees",
+          "View all Departments",
+          "View all Roles",
+          "Add a Department",
+          "Add a Role",
+          "Add an Employee",
+          "Update Employee Role",
+          "Update Manager",
+          "View Employees by Manager",
+          "View Employees by Department",
+          "Delete Department",
+          "Delete Role",
+          "Delete Employee",
+          "View Department Budget",
+        ],
+      },
+    ])
+    .then((data) => {
+      console.log(data.navMenu);
+      if (data.navMenu === "View all Employees") {
+        selectAllEmployees();
+      } else if (data.navMenu === "View all Departments") {
+        selectAllDepartments();
+      } else if (data.navMenu === "View all Roles") {
+        selectAllRoles();
+      }
+    });
+const mainMenu = () => {
+  inquirer
+    .prompt([
+      {
+        type: "confirm",
+        name: "continue",
+        message: "return to main menu?",
+      },
+    ])
+    .then((data) => {
+      if (data.continue) {
+        initInquirer();
+      } else {
+        console.log("Have a nice day!");
+        return;
+      }
+    });
+};
 
-//inquirer.prompt([{}]);
+initInquirer();
--- query.js
+++ query.js
@@ -1,21 +1,34 @@
 const sql = require("mysql2");
 const cTable = require("console.table");
 
-const db = sql.createConnection(
-  {
-    host: "localhost",
-    // MySQL Username
-    user: "root",
+const db = sql.createConnection({
+  host: "localhost",
+  // MySQL Username
+  user: "root",
 
-    password: "password",
-    database: "employee_db",
-  },
-  console.log(`Connected to the employee_db database.`)
-);
+  password: "password",
+  database: "employee_db",
+});
 
 const selectAllEmployees = () =>
-  db.query("SELECT * FROM employee;", function (err, results) {
+  db.query(
+    "SELECT employee.id AS 'Employee ID', employee.first_name AS 'First Name', employee.last_name AS 'Last Name', role.title AS 'Job Title', department.name AS 'Department', role.salary AS 'Salary', concat(manager.first_name,' ',manager.last_name) AS'Manager'FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id INNER JOIN manager ON employee.manager_id = manager.id;",
+    function (err, results) {
+      console.table(results);
+    }
+  );
+
+const selectAllDepartments = () =>
+  db.query("SELECT * FROM department;", function (err, results) {
     console.table(results);
   });
 
-module.exports = { selectAllEmployees };
+const selectAllRoles = () =>
+  db.query(
+    "SELECT role.title AS 'Job Title', role.id as'Role ID', department.name AS 'Department', role.salary AS 'Salary' FROM role INNER JOIN department ON role.department_id = department.id;",
+    function (err, results) {
+      console.table(results);
+    }
+  );
+
+module.exports = { selectAllEmployees, selectAllDepartments, selectAllRoles };
